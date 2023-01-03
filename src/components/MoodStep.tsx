f3b935fa003b534053e284873ef62b3ea989037b
Eleri Ojavere <eleri.ojavere@gmail.com>
2023-01-04 00:35:49 +0200

    memoise random colors

--- src/components/MoodStep.tsx
+++ src/components/MoodStep.tsx
@@ -1,8 +1,7 @@
-import { useForm } from "../context/FormContext";
-import PrimaryButton from "./PrimaryButton";
+import { useMemo, useState } from "react";
 
 export default function MoodStep() {
-  const { changeActiveStepIndex } = useForm();
+  const [activeTile, setActiveTile] = useState<null | string>(null);
 
   const moods = [
     "sad",
@@ -16,20 +15,38 @@ export default function MoodStep() {
 
   const colors = ["green", "blue", "yellow", "purple"];
 
+  const toggleSelection = (mood: string) => {
+    setActiveTile(mood);
+  };
+
+  const computeExpensiveValue = () => {
+    return moods.map((mood) => {
+      return { name: mood, color: Math.floor(Math.random() * colors.length) };
+    });
+  };
+
+  const memoizedValue = useMemo(() => computeExpensiveValue(), []);
+
   return (
     <>
       <div className="font-bold mb-2">How are you feeling today?</div>
       <div className="flex flex-wrap max-w-xl gap-2 ">
-        {moods.map((mood, key) => (
-          <div
-            key={`${mood}-${key}`}
-            className={`w-28 h-28 mood-image text-white rounded justify-center flex items-center  ${
-              colors[Math.floor(Math.random() * colors.length)]
-            }`}
-          >
-            {mood}
-          </div>
-        ))}
+        {moods.map((mood, key) => {
+          const mappedColors = memoizedValue.find(
+            (value) => value.name === mood
+          );
+          return (
+            <button
+              onClick={() => toggleSelection(mood)}
+              key={`${mood}-${key}`}
+              className={`w-28 h-28 mood-image text-white rounded justify-center flex items-center cursor-pointer ${
+                mappedColors ? colors[mappedColors.color] : ""
+              } ${activeTile === mood ? "active" : ""}`}
+            >
+              {mood}
+            </button>
+          );
+        })}
       </div>
     </>
   );
